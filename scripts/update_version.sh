85fae055ef054fa61c1fe4399eab9e8bccc88199
nbesimi <nuhi@adaptivescale.com>
2022-11-02 20:00:36 +0100

    fixed: rosetta version command

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -27,13 +27,15 @@ update_version() {
         return
     fi
     echo "Updating version $APP_VERSION to $VERSION_BUMP"
-    # Replace version in __init__.py
+
     printf '%s\n' "%s/version =.*/version = \'$VERSION_BUMP\'/g" 'x' | ex build.gradle
+    printf '%s\n' "%s/version =.*/version = \"$VERSION_BUMP\",/g" 'x' | ex cli/src/main/java/com/adaptivescale/rosetta/cli/Cli.java
 
     # Commit and push version bump
     git config --local user.name "github-actions[bot]"
     git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
     git add build.gradle
+    git add cli/src/main/java/com/adaptivescale/rosetta/cli/Cli.java
     git commit -m "Version bump: ${BUILD_VERSION} to ${VERSION_BUMP}"
     git push --set-upstream origin $BRANCH_NAME
     APP_VERSION=$VERSION_BUMP
