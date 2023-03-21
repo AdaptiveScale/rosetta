6d92358cb42e2d51007f9747106b009236f2c443
nbesimi <nuhi@adaptivescale.com>
2023-03-21 14:16:23 +0100

    addressed comments

--- cli/src/main/java/com/adaptivescale/rosetta/cli/Main.java
+++ cli/src/main/java/com/adaptivescale/rosetta/cli/Main.java
@@ -5,7 +5,6 @@ import picocli.CommandLine;
 
 public class Main {
     public static void main(String... args) {
-        TranslationMatrix.getInstance();
         int exitCode = new CommandLine(new Cli()).execute(args);
         System.exit(exitCode);
     }
