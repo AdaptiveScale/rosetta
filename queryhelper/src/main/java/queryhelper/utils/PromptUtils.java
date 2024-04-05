be3fb7681e0142e8bcc32be37480e59ee8029ec3
Femi3211 <femi.havziu123@gmail.com>
2024-04-05 15:18:02 +0200

    modified: added to the prompt the dbType from config file to work with all databases

--- queryhelper/src/main/java/queryhelper/utils/PromptUtils.java
+++ queryhelper/src/main/java/queryhelper/utils/PromptUtils.java
@@ -1,14 +1,14 @@
 package queryhelper.utils;
 
+import com.adaptivescale.rosetta.common.models.input.Connection;
 import queryhelper.pojo.QueryRequest;
 public class PromptUtils {
-
-    public static String queryPrompt(QueryRequest queryRequest, String databaseDDL) {
+    public static String queryPrompt(QueryRequest queryRequest, String databaseDDL, Connection source) {
         String query = queryRequest.getQuery();
 
         String outputFormat = FileUtils.readJsonFile();
 
-        return "You are a system that generates and outputs postgresql SQL queries.\n" +
+        return "You are a system that generates and outputs " + source.getDbType() + " SQL queries.\n" +
                 "The following is the DDL of the database:\n\n" +
                 "START OF DDL\n" + databaseDDL + "\nEND OF DDL" +
                 "\n\nI want you to generate a SQL query based on the following description: " +
