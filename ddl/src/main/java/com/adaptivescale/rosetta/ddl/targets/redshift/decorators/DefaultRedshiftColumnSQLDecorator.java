4f9a9e153692a9fa76600e6402c82b3258d485f5
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2024-08-05 20:16:04 +0200

    ADDED: Redshift <-> MySQL

---
+++ ddl/src/main/java/com/adaptivescale/rosetta/ddl/targets/redshift/decorators/DefaultRedshiftColumnSQLDecorator.java
@@ -0,0 +1,30 @@
+package com.adaptivescale.rosetta.ddl.targets.redshift.decorators;
+
+import com.adaptivescale.rosetta.common.models.Column;
+import com.adaptivescale.rosetta.ddl.targets.ColumnDataTypeName;
+import com.adaptivescale.rosetta.ddl.targets.ColumnSQLDecorator;
+
+import static com.adaptivescale.rosetta.ddl.targets.redshift.Constants.DEFAULT_WRAPPER;
+
+public class DefaultRedshiftColumnSQLDecorator implements ColumnSQLDecorator {
+
+    private final Column column;
+    private final ColumnDataTypeName columnDataTypeName;
+
+    public DefaultRedshiftColumnSQLDecorator(Column column, ColumnDataTypeName columnDataTypeName) {
+        this.column = column;
+        this.columnDataTypeName = columnDataTypeName;
+    }
+
+    @Override
+    public String expressSQl() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(DEFAULT_WRAPPER).append(column.getName()).append(DEFAULT_WRAPPER).append(" ");
+        builder.append(columnDataTypeName.nameForColumn(column));
+        if(!column.isNullable()) {
+            builder.append(" NOT NULL ");
+        }
+        return builder.toString();
+    }
+
+}
