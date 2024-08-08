fd5e1edd486dbc911dcc21db527147fdc7ee1121
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2024-08-08 14:06:25 +0200

    ADDED: Test functionality for Redshift

---
+++ test/src/main/java/com/adaptivescale/rosetta/test/assertion/generator/RedshiftAssertionSqlGenerator.java
@@ -0,0 +1,21 @@
+package com.adaptivescale.rosetta.test.assertion.generator;
+
+import com.adaptivescale.rosetta.common.models.Column;
+import com.adaptivescale.rosetta.common.models.Table;
+import com.adaptivescale.rosetta.common.models.input.Connection;
+
+public class RedshiftAssertionSqlGenerator extends BaseAssertionSqlGenerator {
+    @Override
+    String prepareSql(Connection connection, Table table, Column column, String whereClauseCondition) {
+
+        return String.format("Select Count(*) from \"%s\".\"%s\" where %s %s",
+                table.getSchema(),
+                table.getName(),
+                column.getName(),
+                whereClauseCondition);
+    }
+
+    String castValue(Object value, Column column) {
+        return value == null ? null : "'" + value + "'";
+    }
+}
