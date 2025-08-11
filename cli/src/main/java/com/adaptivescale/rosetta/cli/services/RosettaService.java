c843175cb2be9e978c9cb23c71e76f97afd6efc9
jetroni <jetronsaiti@gmail.com>
2025-08-11 11:51:45 +0200

    added: default model yaml name in constants

--- cli/src/main/java/com/adaptivescale/rosetta/cli/services/RosettaService.java
+++ cli/src/main/java/com/adaptivescale/rosetta/cli/services/RosettaService.java
@@ -10,16 +10,17 @@ import org.apache.commons.io.FileUtils;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static com.adaptivescale.rosetta.cli.Constants.DEFAULT_MODEL_YAML_NAME;
+
 @Slf4j
 public class RosettaService {
-    public static final String DEFAULT_MODEL_YAML = "model.yaml";
 
     public void extractModel(Connection connection, Path sourceWorkspace) throws Exception {
         extractModel(connection, sourceWorkspace, false);
     }
 
     public void extractModel(Connection connection, Path sourceWorkspace, boolean forceDelete) throws Exception {
-        Path modelYamlPath = sourceWorkspace.resolve(DEFAULT_MODEL_YAML);
+        Path modelYamlPath = sourceWorkspace.resolve(DEFAULT_MODEL_YAML_NAME);
 
         if (forceDelete) {
             if (Files.exists(sourceWorkspace)) {
@@ -37,7 +38,7 @@ public class RosettaService {
         }
 
         Database result = SourceGeneratorFactory.sourceGenerator(connection).generate(connection);
-        YamlModelOutput yamlInputModel = new YamlModelOutput(DEFAULT_MODEL_YAML, sourceWorkspace);
+        YamlModelOutput yamlInputModel = new YamlModelOutput(DEFAULT_MODEL_YAML_NAME, sourceWorkspace);
         yamlInputModel.write(result);
         log.info("Successfully written input database yaml ({}).", yamlInputModel.getFilePath());
     }
